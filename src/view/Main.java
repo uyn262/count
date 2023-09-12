0b06664ae3a3b664820c08262200bc14c1e329fc
uyen262 <uyn262@gmail.com>
2023-09-12 14:25:19 +0700

    count

---
+++ src/view/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+import controller.CountProgramming;
+/**
+ *
+ * @author Admin
+ */
+public class Main {
+    public static void main(String[] args){
+         new CountProgramming().run();
+    }
+}
